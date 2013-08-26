670b9c35069a8f2cfaf82d06b8ddd989e543a82c
wwwredfish <wwwredfish@gmail.com>
2013-08-26 16:36:01 +0600

    tests for model note updated

--- test/spec/Models/note.js
+++ test/spec/Models/note.js
@@ -8,8 +8,12 @@ define(['scripts/models/note'], function (Model) {
     module('Note model');
     test('Can be created with default values', function(){
         var note = new Model();
-        console.log(note);
         equal(note.get('notebookId'), 0, 'Default notebook id for notes is 0');
         equal(note.get('isFavourite'), 0, 'Defaultly notebook is not favourite');
+    });
+    test('Update attributes', function(){
+        var note = new Model();
+        note.set('title', 'new title');
+        equal(note.get('title'), 'new title');
     });
 });

c7861af5e46645023b2467e822e799741837e4c5
wwebfor <wwebfor@gmail.com>
2014-01-18 03:17:20 +0600

    update

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -6,8 +6,8 @@ define([ ], function () {
     constants.VERSION = '0.0.0';
 
     constants.URL = 'http://laverna.cc/';
-    constants.DROPBOX_KEY = 'io5vfg4w33jx9o4';
-    constants.DROPBOX_SECRET = 'u4rt9wf6id9xbi5';
+    constants.DROPBOX_KEY = '10iirspliqts95d';
+    constants.DROPBOX_SECRET = 'o9h9a2mcn0mte01';
 
     // On localhost
     if (location.hostname.indexOf('localhost') === 0) {

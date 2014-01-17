8cc45735811d63dd30644ef802d5c4816a07b0f9
wwebfor <wwebfor@gmail.com>
2014-01-18 04:14:38 +0600

    0.1.0

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -3,7 +3,7 @@ define([ ], function () {
     'use strict';
 
     var constants = {};
-    constants.VERSION = '0.0.0';
+    constants.VERSION = '0.1.0';
 
     constants.URL = 'http://laverna.cc/';
     constants.DROPBOX_KEY = '10iirspliqts95d';

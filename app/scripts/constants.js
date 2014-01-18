d35ea9473650bc816d75280879558c1988efef84
wwebfor <wwebfor@gmail.com>
2014-01-19 04:08:11 +0600

    https protocol

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -5,7 +5,7 @@ define([ ], function () {
     var constants = {};
     constants.VERSION = '0.1.2';
 
-    constants.URL = 'http://laverna.cc/';
+    constants.URL = 'https://laverna.cc/';
     constants.DROPBOX_KEY = '10iirspliqts95d';
     constants.DROPBOX_SECRET = null;
 

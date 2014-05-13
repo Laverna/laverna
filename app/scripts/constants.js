3116bbcb519f805b82219ca7d70260bd53c7aae9
wwebfor <wwebfor@gmail.com>
2014-05-13 15:57:49 +0600

    Configuration for a root url to host laverna under a subdirectory #77

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -5,14 +5,9 @@ define([ ], function () {
     var constants = {};
     constants.VERSION = '0.5.0';
 
-    constants.URL = 'https://laverna.cc/';
+    constants.URL = location.origin + location.pathname.replace('index.html', '');
     constants.DROPBOX_KEY = '10iirspliqts95d';
     // constants.DROPBOX_SECRET = null;
-
-    // On localhost
-    if (location.hostname.indexOf('localhost') === 0) {
-        constants.URL = location.origin + location.pathname.replace('index.html', '');
-    }
 
     return constants;
 });

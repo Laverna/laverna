9566a24d7ec03b19e45b45e202e21a99c5008c74
wwwredfish <wwwredfish@gmail.com>
2015-05-16 18:34:48 +0600

    :art: Refactor Encryption/encrypt submodule.

--- app/scripts/apps/encryption/encrypt/view.js
+++ app/scripts/apps/encryption/encrypt/view.js
@@ -1,13 +1,17 @@
 /* global define */
 define([
-    'underscore',
     'jquery',
-    'backbone',
+    'underscore',
     'marionette',
-    'text!apps/encryption/encrypt/encryptTemplate.html'
-], function (_, $, Backbone, Marionette, Tmpl) {
+    'backbone.radio',
+    'text!apps/encryption/encrypt/template.html'
+], function($, _, Marionette, Radio, Tmpl) {
     'use strict';
 
+    /**
+     * Re-encryption view.
+     * Shows auth form or progress bar.
+     */
     var View = Marionette.ItemView.extend({
         template: _.template(Tmpl),
 
@@ -16,66 +20,114 @@ define([
         },
 
         ui: {
-            state: '#state',
-            oldPass : 'input[name=oldpass]',
-            password: 'input[name=password]',
-            progress: '#encryption-progress',
-            passForm: '#encryption-password'
+            containerProgress : '#encryption-progress',
+            containerForm     : '#encryption-password',
+
+            progress          : '#progress',
+            state             : '#state',
+
+            // Passwords
+            oldPassword       : 'input[name=oldpass]',
+            password          : 'input[name=password]',
         },
 
-        initialize: function () {
-            this.now = 0;
-            this.max = this.options.notes.length + this.options.notebooks.length;
+        initialize: function() {
+            this.progress = {count: 0, max: 0};
+            this.vent    = Radio.channel('encrypt');
 
-            this.listenTo(this.options.notes, 'reset', this.countMax);
-            this.listenTo(this.options.notebooks, 'reset', this.countMax);
+            // Events
+            this.listenTo(Radio.channel('Encryption'), 'password:valid', this.showProgress);
+            this.listenTo(this, 'encrypt:init', this.changeMax);
 
-            // Change encryption progress
-            this.options.notes.on('encryption:progress', this.progress, this);
-            this.options.notebooks.on('encryption:progress', this.progress, this);
+            this.listenTo(Radio.channel('collection'), 'saved:all', this.saveProgress);
+            this.listenTo(this.vent, 'decrypting:models', this.decryptProgress);
+            this.listenTo(this.vent, 'encrypting:models', this.encryptProgress);
         },
 
-        checkPasswords: function (e) {
-            e.preventDefault();
-            var data = {};
+        changeMax: function(max) {
+            this.progress.max = max;
+        },
 
-            if (this.options.passwords.new) {
-                data.new = this.ui.password.val().trim();
-            }
+        saveProgress: function() {
+            this._setProgress('encryption.state.save');
+        },
 
-            if (this.options.passwords.old) {
-                data.old = this.ui.oldPass.val().trim();
-            }
+        decryptProgress: function() {
+            this._setProgress('encryption.state.decrypt');
+        },
 
-            this.trigger('checkPasswords', data);
+        encryptProgress: function() {
+            this._setProgress('encryption.state.encrypt');
         },
 
         /**
-         * Show encryption progress
+         * Hide auth form and show progress bar.
          */
-        progress: function () {
-            this.ui.passForm.hide();
-            this.ui.progress.removeClass('hide');
-
-            this.now++;
-            this.$('#progress').css('width', ((this.now * 100) / this.max) + '%' );
-            if (this.now === this.max) {
-                this.$('#progress').css('width', '100%' );
-            }
+        showProgress: function() {
+            this.ui.containerForm.hide();
+            this.ui.containerProgress.removeClass('hide');
         },
 
-        serializeData: function () {
+        _setProgress: function(state) {
+            var max = this.progress.max,
+                width;
+
+            // Change progress bar
+            this.progress.count = (
+                this.progress.count >= max ? 1 : this.progress.count + 1
+            );
+            width = Math.floor((this.progress.count * 100) / max);
+            this.ui.progress.css('width', width + '%');
+
+            // Change the status
+            this.ui.state.text($.t(state));
+        },
+
+        checkPasswords: function(e) {
+            e.preventDefault();
+
+            this.trigger('check:passwords', {
+                password : this.ui.password.length ? this.ui.password.val().trim() : null,
+                old      : this.ui.oldPassword.length ? this.ui.oldPassword.val().trim() : null
+            });
+        },
+
+        serializeData: function() {
             return {
-                max: this.max,
-                passwords: this.options.passwords
+                configs: this.options.configs
             };
         },
 
-        templateHelpers: function () {
+        templateHelpers: function() {
             return {
-                i18n: $.t
+                needOldPassword: function() {
+                    return (
+                        (
+                            // Backup password shouldn't be empty
+                            (
+                                !_.isUndefined(this.configs.encryptBackup.encryptPass) &&
+                                this.configs.encryptBackup.encryptPass.length !== 0
+                            ) &&
+                            // Encryption should be enabled
+                            (
+                                _.isUndefined(this.configs.encryptBackup.encrypt) ||
+                                Number(this.configs.encryptBackup.encrypt)
+                            )
+                        ) ||
+                        // Encryption was disabled in new configs
+                        (
+                            !Number(this.configs.encrypt) &&
+                            Number(this.configs.encryptBackup.encrypt)
+                        )
+                    );
+                },
+
+                needNewPassword: function() {
+                    return Number(this.configs.encrypt);
+                }
             };
         }
+
     });
 
     return View;

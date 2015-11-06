e4d539df8e39ecf59c810dcb7facda9d6a3c262e
wwwredfish <wwwredfish@gmail.com>
2015-11-06 15:26:51 +0600

    Make Nightwatch run tests in particular order
    That way we can test a typical user behaviour.

--- test/spec-ui/test.js
+++ test/spec-ui/test.js
@@ -2,12 +2,19 @@
 
 // Test files
 var tests = [
-        './load.js',
-        './apps/notes/form.js',
-        './apps/notebooks/form.js',
-    ];
+    './load.js',
+    './apps/notes/form.js',
+    './apps/notebooks/form.js',
+    './apps/notebooks/list.js',
+];
 
 // Load all test files
 tests.forEach(function(val) {
     require(val);
+});
+
+describe('end session', function() {
+    it('ends session', function(client) {
+        client.end();
+    });
 });

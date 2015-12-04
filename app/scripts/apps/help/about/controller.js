c1869fd0819e130b8badd1dc8b92944a13f3197f
wwebfor <wwebfor@users.noreply.github.com>
2015-12-04 18:50:15 +0600

    Show current release in 'about' page
    Replace hard-coded release version in 'about' page.

--- app/scripts/apps/help/about/controller.js
+++ app/scripts/apps/help/about/controller.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2015 Laverna project Authors.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -10,15 +10,16 @@ define([
     'underscore',
     'marionette',
     'backbone.radio',
-    'apps/help/about/view'
-], function(_, Marionette, Radio, View) {
+    'apps/help/about/view',
+    'constants'
+], function(_, Marionette, Radio, View, constants) {
     'use strict';
 
     var Controller = Marionette.Object.extend({
 
         initialize: function() {
             this.view = new View({
-                appVersion: '0.5'
+                appVersion: constants.VERSION
             });
 
             Radio.request('global', 'region:show', 'modal', this.view);

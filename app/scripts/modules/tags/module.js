3f334d858c0f33f850e470d36b738589fedfdb93
wwwredfish <wwwredfish@gmail.com>
2015-12-10 14:10:32 +0600

    :bug: Fix tag links in different profiles
    Tag links were incorrect in non-default profiles.

--- app/scripts/modules/tags/module.js
+++ app/scripts/modules/tags/module.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2015 Laverna project Authors.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -53,15 +53,17 @@ define([
          * them to a tag link.
          */
         replaceTags: function(text) {
-            var self = this;
+            var self = this,
+                uri;
 
             return text.replace(this.regex, function(tag) {
                 if (!self.testForTag(tag)) {
                     return tag;
                 }
 
-                return ' <a class="label label-default" href="#/notes/f/tag/q/' +
-                    tag.replace('#', '') + '">' + tag + '</a>';
+                uri = Radio.request('uri', 'link:profile', '/notes/f/tag/q/' + tag.replace('#', ''));
+                return ' <a class="label label-default" href="#' + uri +
+                     '">' + tag + '</a>';
             });
         },
 

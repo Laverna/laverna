86f6f54f6f725aa4bf1a93f5b5c449cca88956c1
Aimo Thiele <aimo.thiele@coremedia.com>
2015-12-15 17:18:07 +0100

    Reformat code

--- electron.js
+++ electron.js
@@ -9,14 +9,14 @@ var app               = require('app'),
     BrowserWindow     = require('browser-window'),
     menu              = require('menu');
 
-    var menuTemplate = [{
-            label: "Edit",
-            submenu: [
-                { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-                { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-                { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-            ]}
-        ];
+var menuTemplate = [{
+    label: "Edit",
+    submenu: [
+        {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+        {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+        {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"},
+    ]
+}];
 
 // Start Server
 connect()

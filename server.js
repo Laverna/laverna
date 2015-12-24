aa1c0e4f7d0ac831938979293bba5d1455cd37d3
wwwredfish <wwwredfish@gmail.com>
2015-12-24 19:33:21 +0600

    Switch to connect

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 'use strict';
 const PORT  = 9100;
-var nStatic = require('node-static'),
-    http    = require('http'),
-    file    = new nStatic.Server('./dist');
+var connect = require('connect');
 
-http.createServer(function(req, res) {
-    file.serve(req, res);
-}).listen(PORT);
+connect()
+.use(connect.static(__dirname + '/dist'))
+.listen(PORT);
 
 console.log('Server is running on port: ' + PORT);

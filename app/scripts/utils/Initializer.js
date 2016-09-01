dcc654c12a7090d689f2cb81ab28bdd341220e3d
wwwredfish <wwwredfish@gmail.com>
2016-09-01 15:33:17 +0600

    [Refactor] :art: Rewrite utils/Initializer to ES6
    Changes:
    * :art: Rewrite to ES6
    * :memo: Properly document the changes

--- app/scripts/utils/Initializer.js
+++ app/scripts/utils/Initializer.js
@@ -1,89 +1,110 @@
+import _ from 'underscore';
+import Radio from 'backbone.radio';
+import deb from 'debug';
+
+const log = deb('lav:utils/Initializer');
+
 /**
- * Copyright (C) 2015 Laverna project Authors.
+ * Add and execute asynchronous callbacks which can be
+ * started later by requesting them with Backbone.Radio.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * @example
+ * // Add a new callback to an array of callbacks.
+ * // It can be found later by the key app:before
+ * Radio.request('utils/Initializer', 'add', 'app:before', promise);
+ * @example
+ * // Start the previously added callbacks.
+ * // In this example it executes all callbacks with the key app:before
+ * Radio.request('utils/Initializer', 'start', 'app:before', options);
+ * @module Initializer
+ * @class
+ * @license MPL-2.0
  */
-/* global define */
-define([
-    'underscore',
-    'q',
-    'backbone.radio',
-    'marionette'
-], function(_, Q, Radio, Marionette) {
-    'use strict';
+export default class Initializer {
 
     /**
-     * This class is used to add and execute asynchronous initializers.
-     * It is very convenient when writing modules or writing sub apps.
-     * Also it ensures that every module or sub app is ready
-     * before the app starts.
+     * Channel to which it replies and triggers events.
      *
-     * App initializers will be executed first. Then, module initializers.
-     *
-     * Adding new initializers:
-     * Radio.request('init', 'add', '[app|app:before|module]', function() {});
-     *
-     * Executing initializers
-     * Radio.request('init', 'start', '[app|app:before|module]', args);
+     * @returns {Object} Backbone.Radio object
      */
-    var Initializers = Marionette.Object.extend({
+    get channel() {
+        return Radio.channel('utils/Initializer');
+    }
 
-        initialize: function() {
-            this._inits = {};
+    /**
+     * Constructor.
+     *
+     * @listens utils/Initializer#add - schedule a callback
+     * @listens utils/Initializer#start - execute scheduled callbacks
+     * @fires utils/Initializer#init
+     */
+    constructor() {
+        this._inits = {};
 
-            Radio.channel('init')
-            .reply('add', this.addInit, this)
-            .reply('start', this.executeInits, this);
-        },
-
-        addInit: function(name, initializer) {
-            this._inits[name] = this._inits[name] || [];
-            this._inits[name].push(initializer);
-        },
-
-        /**
-         * Executes all the initializers
-         */
-        executeInits: function(types, args) {
-            var self  = this;
-
-            types = types.split(' ');
-            args  = Array.prototype.slice.call(arguments, 1);
-
-            return function() {
-                var promises = [];
-
-                // Execute every init one after another
-                _.each(types, function(type) {
-                    promises.push(function() {
-                        return self._executeInit(type, args);
-                    });
-                });
-
-                return _.reduce(promises, Q.when, new Q());
-            };
-        },
+        // Start replying to requests
+        this.channel.reply({
+            add   : this.add,
+            start : this.start,
+        }, this);
 
         /**
-         * Executes an initializer
+         * Initializer is ready to schedule callbacks.
+         *
+         * @event utils/Initializer#init
          */
-        _executeInit: function(type, args) {
-            var self     = this,
-                promises = [];
+        this.channel.trigger('init');
+    }
 
-            // Execute all the functions asynchronously
-            _.each(self._inits[type], function(fnc) {
-                promises.push(function() {
-                    return new Q(fnc.apply(null, args));
-                });
-            });
+    /**
+     * Schedule a callback.
+     * i.e. attach to an array of callbacks.
+     *
+     * @param {String} name - name of the initializer
+     * @param {Function} initializer
+     */
+    add(name, initializer) {
+        this._inits[name] = this._inits[name] || [];
+        this._inits[name].push(initializer);
+        log(`added a callback to ${name}`);
+    }
 
-            return _.reduce(promises, Q.when, new Q());
-        }
+    /**
+     * Start an initializer(s).
+     * i.e. execute all scheduled callbacks with a key name/s.
+     *
+     * @param {String} name - name(s) of the initializer
+     * @param {Object} options - options for scheduled callbacks
+     * @returns {Promise} it will be resolved once all callbacks complete their tasks
+     */
+    start(name, options) {
+        const names   = name.split(' ');
+        const promise = Promise.resolve();
+        log(`starting ${name} initializer/s`);
 
-    });
+        _.each(names, name => {
+            promise.then(() => this.startInit(name, options));
+        });
 
-    return new Initializers();
-});
+        return promise;
+    }
+
+    /**
+     * Execute all callbacks of a particular initializer.
+     * i.e. callbacks that are saved in this._inits[name].
+     *
+     * @param {String} name - name of an initializer
+     * @param {Object} options
+     * @returns {Promise} it will be resolved once all callbacks complete their tasks
+     */
+    startInit(name, options) {
+        const promises = [];
+
+        // Get all callbacks by key name and execute
+        _.each(this._inits[name], fnc => {
+            promises.push(fnc(options));
+        });
+
+        return Promise.all(promises);
+    }
+
+}

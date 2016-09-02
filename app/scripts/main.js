574907448e78c0acd2faacb4a48592fe75b79b0d
wwebfor <wwebfor@users.noreply.github.com>
2016-09-02 17:13:32 +0600

    [Refactor] Rewrite utils/underscore to ES6
    Changes:
    * Add xss package to NPM dependencies
    * Rewrite utils/underscore.js to ES6
    * :memo: Document everything

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,7 @@ import App from './App';
 
 // Utils
 import './utils/Initializer';
+import './utils/underscore';
 
 const log = deb('lav:main');
 
--- app/scripts/utils/underscore.js
+++ app/scripts/utils/underscore.js
@@ -1,75 +1,83 @@
+import _ from 'underscore';
+import xss from 'xss';
+
 /**
- * Copyright (C) 2015 Laverna project Authors.
+ * Customize underscore template engine.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * @namespace templateSettings
+ * @property {RegExp} escape - {{ myVar }} show escaped string
+ * @property {RegExp} interpolate - {= myVar } - show string without escaping
+ * @property {RegExp} evaluate - <% code %> - execute JS code
  */
-/* global define */
-define([
-    'underscore',
-    'xss'
-], function(_, cleanXSS) {
-    'use strict';
+_.templateSettings = {
+    escape      : /\{\{([\s\S]+?)\}\}/g,
+    interpolate : /\{=([\s\S]+?)\}/g,
+    evaluate    : /<%([\s\S]+?)%>/g,
+};
+
+/**
+ * Add some helper functions to Underscore.
+ *
+ * @module Underscore
+ * @license MPL-2.0
+ */
+_.mixin({
 
     /**
-     * Add some helper functions to Underscore.
+     * Sanitize HTML to prevent XSS.
+     *
+     * @example
+     * _.cleanXSS('<script>alert('yes');</script>Test'); // Result - 'test'
+     * @example
+     * _.cleanXSS('<script></script><b>Test</b>', true, true); // Result - 'test'
+     * @param {String} str - text that needs to be sanitized
+     * @param {Boolean} unescape - true if there are some escaped special characters
+     * @param {Boolean} stripTags - true if you want to remove HTML tags from the text
+     * @returns {String}
      */
-    _.mixin({
+    cleanXSS(text, unescape, stripTags) {
+        let str = text;
 
-        /**
-         * Sanitize HTML to prevent XSS.
-         *
-         * @type string str
-         * @type boolean unescape
-         * @type boolean stripTags
+        /*
+         * Unescape the string 2 times because
+         * data from Dropbox is always escaped + we escape them too.
          */
-        cleanXSS: function(str, unescape, stripTags) {
+        if (unescape === true) {
+            str = _.runTimes(_.unescape, 2, str);
+        }
 
-            /* Unescape the string 2 times because
-             * data from Dropbox is always escaped + we escape them too.
-             */
-            if (unescape === true) {
-                str = _.runTimes(_.unescape, 2, str);
-            }
+        // Remove all HTML tags
+        if (stripTags === true) {
+            str = _.stripTags(str);
+        }
 
-            // Remove all HTML tags
-            if (stripTags === true) {
-                str = _.stripTags(str);
-            }
+        return xss(str);
+    },
 
-            return cleanXSS(str);
-        },
+    /**
+     * Invoke the given function n times and return the last result.
+     *
+     * @example _.runTimes(_.unescape, 2, 'String');
+     * @param {Function} fnc - a callback function
+     * @param {Number} n - number of times the callback should run
+     * @returns {} result of running a callback n times
+     */
+    runTimes(fnc, n, ...args) {
+        const res = _.times(n, () => fnc.apply(null, args));
+        return res[res.length - 1];
+    },
 
-        /**
-         * Invokes the given function n times and returns the last result.
-         * Example:
-         * _.runTimes(_.unescape, 2, 'String');
-         *
-         * @type function func
-         * @type number n
-         */
-        runTimes: function(func, n) {
-            var args = Array.prototype.slice.call(arguments, 2),
-                res;
+    /**
+     * Remove HTML tags from string.
+     *
+     * @example _.stripTags('<b>Bold</b>'); // Resulting text: 'Bold'
+     * @param {String} str
+     * @returns {String}
+     */
+    stripTags(str) {
+        return str.replace(/<\/?[^>]+>/g, '');
+    },
 
-            res = _.times(n, function() {
-                return func.apply(null, args);
-            });
-
-            return res[res.length - 1];
-        },
-
-        /**
-         * Remove all HTML from string.
-         *
-         * @type string str
-         */
-        stripTags: function(str) {
-            return str.replace(/<\/?[^>]+>/g, '');
-        },
-
-    });
-
-    return _;
 });
+
+export default _;

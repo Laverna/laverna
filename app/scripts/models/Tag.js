1dbaacdc201c7942639d979a1c118042621cab2a
wwebfor <wwebfor@users.noreply.github.com>
2016-09-17 16:41:58 +0600

    [Refactor] Rewrite models/Tag to ES6

---
+++ app/scripts/models/Tag.js
@@ -0,0 +1,59 @@
+import Model from './Model';
+
+/**
+ * Tag model.
+ *
+ * @class
+ * @extends Model
+ * @license MPL-2.0
+ */
+class Tag extends Model {
+
+    get storeName() {
+        return 'tags';
+    }
+
+    /**
+     * Default values.
+     *
+     * @property {String} type - type of data stored in this model
+     * @property {(String|Undefined)} id - undefined for default
+     * @property {String} name - the name of a tag
+     * @property {Number} count - the number of notes attached to a tag
+     * @property {Number} trash
+     * @property {Date.now()} created
+     * @property {Date.now()} updated
+     * @returns {Object}
+     */
+    get defaults() {
+        return {
+            type    : 'tags',
+            id      : undefined,
+            name    : '',
+            count   : 0,
+            trash   : 0,
+            created : 0,
+            updated : 0,
+        };
+    }
+
+    /**
+     * Attributes that need to be encrypted.
+     *
+     * @returns {Array}
+     */
+    get encryptKeys() {
+        return ['name'];
+    }
+
+    get validateAttributes() {
+        return ['name'];
+    }
+
+    get escapeAttributes() {
+        return ['name'];
+    }
+
+}
+
+export default Tag;

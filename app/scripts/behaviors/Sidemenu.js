c6c5f8534cfd791a7eee2ca0615a8ea68d42bf83
wwebfor <wwebfor@users.noreply.github.com>
2016-11-10 17:28:17 +0600

    [Refactor] Rewrite behaviors/Sidemenu to ES6

--- app/scripts/behaviors/Sidemenu.js
+++ app/scripts/behaviors/Sidemenu.js
@@ -1,91 +1,118 @@
 /**
- * Copyright (C) 2015 Laverna project Authors.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * @module behaviors/Sidemenu
  */
-/* global define */
-define([
-    'underscore',
-    'jquery',
-    'marionette',
-    'backbone.radio',
-    'mousetrap'
-], function(_, $, Marionette, Radio, Mousetrap) {
-    'use strict';
+import Mn from 'backbone.marionette';
+import _ from 'underscore';
+import $ from 'jquery';
+import Hammer from 'hammerjs';
+import Mousetrap from 'mousetrap';
+import Radio from 'backbone.radio';
 
-    /**
-     * This behaviour can be used for views which need to show
-     * hamburger type of menu.
-     */
-    var Sidemenu = Marionette.Behavior.extend({
+/**
+ * Hamburger menu behavior.
+ *
+ * @class
+ * @extends Marionette.Behavior
+ * @license MPL-2.0
+ */
+export default class Sidemenu extends Mn.Behavior {
 
-        ui: {
-            sidemenu: '.sidemenu'
-        },
+    ui() {
+        return {
+            sidemenu: '.sidemenu',
+        };
+    }
 
-        defaults: {
-            events: {
-                'swipeleft'  : 'hideMenu',
-            }
-        },
-
-        events: {
+    events() {
+        return {
             'click .sidemenu--open'  : 'showMenu',
             'click .sidemenu--close' : 'hideMenu',
-            'click .sidemenu a'      : 'hideMenu'
-        },
+            'click .sidemenu a'      : 'hideMenu',
+        };
+    }
 
-        initialize: function() {
-            this.listenTo(Radio.channel('sidemenu'), 'show', this.showMenu);
-        },
+    initialize() {
+        /**
+         * Hammer events. If the view has smenuHammerEvents property,
+         * it will override the behavior's.
+         *
+         * @prop
+         * @prop {String} swiperight - show the menu
+         * @prop {String} swipeleft - hide the menu
+         */
+        this.hammerEvents = this.view.smenuHammerEvents || {
+            swiperight: 'showMenu',
+            swipeleft : 'hideMenu',
+        };
 
-        onRender: function() {
+        this.listenTo(this.view.channel, 'show:sidemenu', this.showMenu);
+        this.listenTo(Radio.channel('utils/Keybindings'), 'appShowSidemenu',
+            this.showMenu);
+    }
 
-            // To avoid bugginess, add hammer events to the backdrop el too
-            var hammer  = $('.layout--backdrop').hammer(),
-                hammer2 = this.$el.hammer();
-
-            _.each(this.options.events, function(func, ev) {
-                hammer.bind(ev,  _.bind(this.view[func] || this[func], this));
-                hammer2.bind(ev, _.bind(this.view[func] || this[func], this));
-            }, this);
-        },
-
-        onShow: function() {
-            _.bindAll(this, 'hideMenu');
-
-            this.$backdrop = $('#layout--backdrop');
-
-            // Hide when 'Esc' was pressed
-            Mousetrap.bind('esc', this.hideMenu);
-        },
-
-        hideMenu: function() {
-            this.ui.sidemenu.removeClass('-show');
-            this.$backdrop.removeClass('-show');
-        },
-
-        showMenu: function() {
-            var self = this;
-
-            // Show the menu and backdrop
-            this.ui.sidemenu.addClass('-show');
-            this.$backdrop.addClass('-show');
-
-            this.ui.sidemenu.scrollTop(0);
-
-            // Hide the menu when a user clicks on the backdrop area
-            this.$backdrop.on('click', function() {
-                self.$backdrop.off('click');
-                self.hideMenu();
-            });
-
-            return false;
+    onDestroy() {
+        if (this.hammer) {
+            this.hammer.destroy();
+            this.hammer2.destroy();
         }
+    }
 
-    });
+    onRender() {
+        this.$backdrop = $('#layout--backdrop');
+        this.listenToHammer();
+    }
 
-    return Sidemenu;
-});
+    /**
+     * Listen to hammerjs events.
+     */
+    listenToHammer() {
+        // To avoid bugginess, add hammer events to the backdrop el too
+        this.hammer  = new Hammer(this.$backdrop[0]);
+        this.hammer2 = new Hammer(this.$el[0]);
+
+        _.each(this.hammerEvents, (method, evt) => {
+            const func = _.bind(this.view[method] || this[method], this);
+            this.hammer.on(evt, func);
+            this.hammer2.on(evt, func);
+        });
+    }
+
+    /**
+     * Show the menu.
+     */
+    showMenu() {
+        // Show the menu and backdrop
+        this.ui.sidemenu.addClass('-show');
+        this.$backdrop.addClass('-show');
+
+        // Reset the scroll position
+        this.ui.sidemenu.scrollTop(0);
+
+        // Hide the menu if Escape is pressed
+        Mousetrap.bind('esc', () => this.hideMenu());
+
+        this.$backdrop.on('click', () => this.onBackdropClick());
+        return false;
+    }
+
+    /**
+     * Hide the menu if the backdrop is clicked.
+     */
+    onBackdropClick() {
+        this.hideMenu();
+        this.$backdrop.off('click');
+    }
+
+    /**
+     * Hide the menu.
+     */
+    hideMenu(e) {
+        // Don't prevent the default behavior if a link is clicked
+        if (e && !$(e.currentTarget).hasClass('sidemenu--item')) {
+            e.preventDefault();
+        }
+        this.ui.sidemenu.removeClass('-show');
+        this.$backdrop.removeClass('-show');
+    }
+
+}

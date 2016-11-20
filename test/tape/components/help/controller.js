c69960e044dd79f56d2df0afdc128c6b54958174
wwebfor <wwebfor@users.noreply.github.com>
2016-11-20 14:17:05 +0600

    [Test] Cover help/keybindings with tests

--- test/tape/components/help/controller.js
+++ test/tape/components/help/controller.js
@@ -5,8 +5,12 @@
 import test from 'tape';
 import sinon from 'sinon';
 import Radio from 'backbone.radio';
+
+/* eslint-disable */
 import controller from '../../../../app/scripts/components/help/controller';
 import About from '../../../../app/scripts/components/help/about/Controller';
+import Keybindings from '../../../../app/scripts/components/help/keybindings/Controller';
+/* eslint-enable */
 
 let sand;
 test('help/controller: before()', t => {
@@ -16,6 +20,7 @@ test('help/controller: before()', t => {
 
 test('help/controller: init()', t => {
     const reply = sand.stub(Radio, 'reply');
+    const on    = sand.stub(Radio, 'on');
 
     controller.init();
     t.equal(reply.calledWith('components/help', {
@@ -23,6 +28,9 @@ test('help/controller: init()', t => {
         showFirstStart  : controller.showFirstStart,
         showKeybindings : controller.showKeybindings,
     }, controller), true, 'starts replying to requests');
+
+    t.equal(on.calledWith('utils/Keybindings', 'appKeyboardHelp'), true,
+        'shows keybinding help if "?" key is pressed');
 
     sand.restore();
     t.end();
@@ -33,6 +41,16 @@ test('help/controller: showAbout', t => {
 
     controller.showAbout();
     t.equal(init.called, true, 'shows about page');
+
+    sand.restore();
+    t.end();
+});
+
+test('help/controller: showKeybindings', t => {
+    const init = sand.stub(Keybindings.prototype, 'init');
+
+    controller.showKeybindings();
+    t.equal(init.called, true, 'shows keybindings page');
 
     sand.restore();
     t.end();

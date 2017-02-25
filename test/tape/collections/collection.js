3a343449a315e5c1980ea5441e1c8aa3a4af9336
wwebfor <wwebfor@users.noreply.github.com>
2017-02-25 13:18:01 +0600

    [Test] Update tests for collections/Collection

--- test/tape/collections/collection.js
+++ test/tape/collections/collection.js
@@ -3,6 +3,7 @@
  */
 import test from 'tape';
 import sinon from 'sinon';
+import '../../../app/scripts/utils/underscore';
 import Collection from '../../../app/scripts/collections/Collection';
 import Note from '../../../app/scripts/models/Note';
 
@@ -48,6 +49,13 @@ test('Collection: storeName', t => {
     t.end();
 });
 
+test('Collection: channel', t => {
+    const coll = new Collection();
+    coll.model = Note.extend({});
+    t.equal(coll.channel, coll.model.prototype.channel);
+    t.end();
+});
+
 test('Collection: filterList()', t => {
     const coll      = new Collection();
     coll.testFilter = sand.stub().returns([]);
@@ -89,6 +97,7 @@ test('Collection: findOrCreate()', t => {
     coll.add({id: 1});
 
     t.equal(typeof coll.findOrCreate(2), 'object', 'returns an object');
+    t.equal(coll.findOrCreate(3).get('id'), 3, 'creates a new model');
     t.equal(coll.findOrCreate(1), coll.get(1), 'finds the model in the collection');
 
     t.end();

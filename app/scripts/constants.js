37d76dc73ea0583c99f8095d35965061a799fc3e
Oleg Fiksel <oleg.fiksel@accenture.com>
2018-08-14 09:16:17 +0200

    Version bump

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -11,7 +11,7 @@ define(['underscore'], function (_) {
 
     var constants = {};
 
-    constants.VERSION = '0.7.53';
+    constants.VERSION = '0.7.54';
     constants.URL = location.origin + location.pathname.replace('index.html', '');
 
     // List of hosts and urls where default dropbox API will work
